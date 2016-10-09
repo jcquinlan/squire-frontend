71aa49c73d450ab1c137b2c22b802135cc849c2e
James Quinlan <jquinlan@bricksimple.com>
2016-10-08 23:04:18 -0400

    Increase expiration time for tokens to 2 hours

--- lackey/settings.py
+++ lackey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -53,6 +54,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+}
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=7200),
 }
 
 MIDDLEWARE = [
